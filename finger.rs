c1d9653da03cae9daaa52af06a157d52ca2971c7
GR4P3 <34196370+aimanpoji@users.noreply.github.com>
2019-06-12 22:22:27 +0800

    Update finger.rs

--- finger.rs
+++ finger.rs
@@ -2,9 +2,18 @@ use std::io::Command;
 
 fn main() {
     // Spawn a process, wait for it to finish, and collect it's output
-    let the_output = Command::new("finger").output()
-        .ok().expect("Failed to execute.");
-    // Encode the resulting data.
-    let encoded = String::from_utf8_lossy(the_output.output.as_slice());
-    print!("{}", encoded);
+    let mut cmd = Command::new("finger");
+    cmd.arg("root");
+     //execute command
+    match cmd.output(){
+    ok(o) => {
+        //else
+        unsafe {
+            println!("Output: {}",String::from_utf8_unchecked(o.stdout));
+        }
+        
+    }
+    Err(e) => {
+        println!("there is an error",e);
+        }
 }
